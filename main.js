593c51c523341142d3eadcc0abb932fb419c22e0
Beatrize <heybeatrize@gmail.com>
2018-05-26 16:28:20 -0700

    css edits

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-$(".header__icons i").on("click", function() {
+/* Animation for nav bar icons when clicked - may not keep */
+/* $(".header__icons i").on("click", function() {
     TweenMax.fromTo(
     $(this),
     1,
@@ -11,4 +12,30 @@ $(".header__icons i").on("click", function() {
         y: 0
     }
 )
-});
+}); */
+
+/* Animation for header arrow */
+let tl = new TimelineMax({ repeat: -1, repeatDelay: 3 })
+
+ tl.fromTo(
+$(".mobile-arrow"),
+    0.8,
+    {
+        ease:  SlowMo.ease.config(0.7, 0.4, false), 
+        y: 0
+        
+    }, 
+    {
+        ease:  Power0.ease, 
+        y: -25,
+        rotation: 720 
+    } 
+)
+    .to(
+ $(".mobile-arrow"),
+    1,
+     {
+        ease:  Elastic.easeOut.config(1.5, 0.3), 
+        y: 0
+     }
+ )
